20c76ef4cb48b6d501a524b50ad1315447caf978
Arun Sasi <aruntdpa@gmail.com>
2025-09-01 12:20:41 +0200

    Demo version with a string publisher

--- src/warehouse_robot/src/warehouse_publisher_node.cpp
+++ src/warehouse_robot/src/warehouse_publisher_node.cpp
@@ -1,15 +1,36 @@
 #include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/string.hpp"
 
+
+using String = example_interfaces::msg::String;
 
 class RobotPublisherNode: public rclcpp::Node
 {
 public:
     RobotPublisherNode(): Node("robot_publisher")
     {
+        publisher_ = create_publisher<String>(
+            "warehouse_test", 
+            10
+        );
+        timer_ = create_wall_timer(
+            std::chrono::seconds(1),
+            std::bind(&RobotPublisherNode::publishMsg, this)
+        );
 
     }
 
 private:
+
+    void publishMsg(){
+        auto msg = String();
+        msg.data = "Test";
+        publisher_->publish(msg);
+        RCLCPP_INFO(get_logger(), "Publishing a message");
+    }
+
+    rclcpp::Publisher<String>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
 
 };
 
