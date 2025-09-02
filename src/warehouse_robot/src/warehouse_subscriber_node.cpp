e9c524d01b55baaf9e383b87627f63ef764188f4
Arun Sasi <aruntdpa@gmail.com>
2025-09-02 19:50:09 +0200

    Implementing minimum_subscriber

--- src/warehouse_robot/src/warehouse_subscriber_node.cpp
+++ src/warehouse_robot/src/warehouse_subscriber_node.cpp
@@ -3,17 +3,28 @@
 
 using String = example_interfaces::msg::String;
 
+using  namespace std::placeholders;
+
 class RobotSubscriberNode: public rclcpp::Node
 {
 public:
     RobotSubscriberNode(): Node("robot_subscriber")
     {
-        subsriber_ = create_
+        subscriber_ = create_subscription<String>(
+            "warehouse_test", 
+            10,
+            std::bind(&RobotSubscriberNode::subscriptionCallBack, this, _1)
+        );
+        RCLCPP_INFO(get_logger(), "Started the subscriber");
 
     }
 
-private:
-    rclcpp::Subscription<Empty>::SharedPtr subscriber_;
+private:   
+    void subscriptionCallBack(String msg){
+        RCLCPP_INFO(get_logger(), "Received a message");
+    }
+
+    rclcpp::Subscription<String>::SharedPtr subscriber_;
 };
 
 int main(int argc, char **argv)
